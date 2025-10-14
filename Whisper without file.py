f759cec41d6253d7d1fede2640fe7af8ca04cce6
egor230 <eglve@mail.ru>
2025-10-14 05:55:45 +0500

    Add or update .py, .txt, .sh files

--- Whisper without file.py
+++ Whisper without file.py
@@ -1,13 +1,8 @@
-import os
-import sys, time
-import numpy as np
-import sounddevice as sd
+from write_text import *
 import webrtcvad
-from queue import Queue
 from collections import deque
 from faster_whisper import WhisperModel
 import scipy.io.wavfile as wavfile
-# Paths and model loading
 cache_dir = "/mnt/807EB5FA7EB5E954/soft/Virtual_machine/linux must have/python_linux/work/cache/whisper/"
 model_size = "large-v3"
 model_dir = os.path.join(cache_dir, f"models--Systran--faster-whisper-{model_size}", "snapshots")
@@ -29,41 +24,39 @@ except Exception as e:
     sys.exit(1)
 
 # Функция для потоковой записи аудио
-def record_audio_stream(fs=16000, chunk_duration=0.03):
-    queue = Queue()
-    def callback(indata, frames, time_info, status):
-        if status:
-            print(f"Статус потока: {status}")
-        queue.put(indata.copy())
+def record_audio_stream(fs=48000, chunk_duration=0.03):
+  queue = Queue()
+  def callback(indata, frames, time_info, status):
+   if status:
+       print(f"Статус потока: {status}")
+   queue.put(indata.copy())
 
-    stream = sd.InputStream(samplerate=fs, channels=1, callback=callback, blocksize=int(fs * chunk_duration))
-    return stream, queue
-import threading
-from scipy.io import wavfile
+  stream = sd.InputStream(samplerate=fs, channels=1, callback=callback, blocksize=int(fs * chunk_duration))
+  return stream, queue
 
 def transcribe_audio_segment(speech_segment, model):
- try:
-   # Сохраняем аудио-сегмент во временный файл
-   filename = "temp_speech_segment.wav"
-   wavfile.write(filename, 16000, np.int16(speech_segment * 32767))
+ try:   # Сохраняем аудио-сегмент во временный файл
+  filename = "temp.wav"
+  wavfile.write(filename, 48000, np.int16(speech_segment * 32767))
+   # Проверка звука
+  if is_speech():
    # Транскрибируем аудио
-   segments, info = model.transcribe(filename, beam_size=10,
-                                     language="ru",  vad_filter=False, temperature=0.8)
+   segments, info = model.transcribe(filename, beam_size=10, language="ru",
+                                     vad_filter=True, temperature=0.7, condition_on_previous_text=True)
    segments_list = list(segments)
    if not segments_list:
-     print("Whisper не вернул сегментов")
+     return
    else:
     message = " ".join([seg.text.strip() for seg in segments_list])
     if message:
-     print(f"Распознанный текст: {message}")
+     message = repeat(message)  # Автоподгонка ширины окна
+     threading.Thread(target=process_text, args=(message,), daemon=True).start()
      
  except Exception as e:
    print(f"Ошибка при транскрипции: {e}")
 
-# Функция обработки аудиопотока
-def process_audio_stream(queue):
- # Инициализация параметров
- vad = webrtcvad.Vad(3)
+def process_audio_stream(queue):# Функция обработки аудиопотока
+ vad = webrtcvad.Vad(3) # Инициализация параметров
  buffer = deque()
  silence_time = 0
  start_time = time.time()
@@ -78,11 +71,10 @@ def process_audio_stream(queue):
    if audio_chunk is None:#   print("Завершение работы...")
     break
    audio_int16 = np.int16(audio_chunk * 32767)
-   is_speech_chunk = vad.is_speech(audio_int16.tobytes(), sample_rate=16000)
+   is_speech_chunk = vad.is_speech(audio_int16.tobytes(), sample_rate=48000)
    current_time = time.time()
    buffer.append(audio_chunk)
-   # Обнаружение начала речи
-   if is_speech_chunk and not speech_detected:
+   if is_speech_chunk and not speech_detected:   # Обнаружение начала речи
     speech_segment = np.concatenate(buffer).astype(np.float32)
     if np.max(np.abs(speech_segment)) > 0.7:
      print("Начало записи (обнаружена речь)")
@@ -95,21 +87,15 @@ def process_audio_stream(queue):
     else: # Тишина — накапливаем время
      silence_time += current_time - last_speech_time
      last_speech_time = current_time
-     # Проверка на окончание речи
-     if silence_time > 0.3:
-      print(f"Время тишины: {silence_time:.2f} сек")
-      # Сброс
    if (speech_detected and silence_time > min_silence_duration ) and buffer: # Проверяем, что буфер не пустой
     speech_segment = np.concatenate(buffer).astype(np.float32)  # Финальный сегмент
-    segment_duration = len(speech_segment) / 16000
+    segment_duration = len(speech_segment) / 48000
     print(f" длительность: {segment_duration:.2f} сек ")
     speech_detected = False
     silence_time = 0
-    # input()
     t1 = threading.Thread(target=transcribe_audio_segment, args=(speech_segment, model,))# Поток завершится при завершении основной программы
     t1.start()
     buffer.clear()  # Сбрасываем буфер
-    input()
 # Запуск
 stream, queue = record_audio_stream()
 stream.start()
--- write_text.py
+++ write_text.py
@@ -1,10 +1,11 @@
-import sys, os, subprocess, json, queue, wave, io, threading, re, time
+import sys, os, subprocess, json,  wave, io, threading, re, time
 from scipy.io.wavfile import write
 os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = "myenv/lib/python3.12/site-packages/PyQt5/Qt5/plugins"
 from PyQt5 import QtCore, QtWidgets, QtGui  # Импорт необходимых модулей из PyQt5
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QSystemTrayIcon, QAction, QMenu, QWidget, QDialog, QLabel, QSystemTrayIcon, QMenu, QAction, QVBoxLayout, QPushButton, QApplication
 from PyQt5.QtCore import QTimer
+from queue import Queue
 import sounddevice as sd
 import tkinter as tk
 from tkinter import Frame, Label
