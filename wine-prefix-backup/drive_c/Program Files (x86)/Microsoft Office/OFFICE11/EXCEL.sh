64c6c6f0f52d41a6760269877692e92386377c0a
egor230 <eglive@yandex.ru>
2026-06-26 18:26:19 +0500

    Обновлён .gitignore (скрипт и чувствительные папки исключены)

--- wine-prefix-backup/drive_c/Program Files (x86)/Microsoft Office/OFFICE11/EXCEL.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export WINEDLLOVERRIDES="d3d8,d3d9,ddraw,dinput8,dsound=n,b";
-export PW_VULKAN_USE=0;
-export LAUNCH_PARAMETERS="";
-wine "/home/egor/.wine/drive_c/Program Files (x86)/Microsoft Office/OFFICE11/EXCEL.EXE" #--fullscreen;
-exit; 
--- wine-prefix-backup/drive_c/Program Files (x86)/Microsoft Office/OFFICE11/WINWORD.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export WINEDLLOVERRIDES="d3d8,d3d9,ddraw,dinput8,dsound=n,b";
-export PW_VULKAN_USE=0;
-export LAUNCH_PARAMETERS="";
-export WINE_SIMULATE_WRITE=COPY wine "/home/egor/.wine/drive_c/Program Files (x86)/Microsoft Office/OFFICE11/WINWORD.EXE";
-wine "/home/egor/.wine/drive_c/Program Files (x86)/Microsoft Office/OFFICE11/WINWORD.EXE" #--fullscreen;
-exit; 
